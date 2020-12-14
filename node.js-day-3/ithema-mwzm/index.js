4fbba9731d956393491b796cb0da8f35df06b277
xiaoze <1416065820@qq.com>
2020-12-14 17:49:18 +0800

    npm

---
+++ node.js-day-3/ithema-mwzm/index.js
@@ -0,0 +1,56 @@
+// 定义格式化时间的函数
+function dateFormat(dateStr) {
+    const dt = new Date(dateStr)
+
+    const y = dt.getFullYear()
+    const m = padZero(dt.getMonth() + 1)
+    const d = padZero(dt.getDate())
+
+    const hh = padZero(dt.getHours())
+    const mm = padZero(dt.getMinutes())
+    const ss = padZero(dt.getSeconds())
+
+    return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+}
+// 定义转义 HTML 字符的函数
+function htmlEscape(htmlstr) {
+    return htmlstr.replace(/<|>|"|&/g, match => {
+        switch (match) {
+            case '<':
+                return '&lt;'
+            case '>':
+                return '&gt;'
+            case '"':
+                return '&quot;'
+            case '&':
+                return '&amp;'
+        }
+    })
+}
+
+// 定义还原 HTML 字符串的函数
+function htmlUnEscape(str) {
+    return str.replace(/&lt;|&gt;|&quot;|&amp;/g, match => {
+        switch (match) {
+            case '&lt;':
+                return '<'
+            case '&gt;':
+                return '>'
+            case '&quot;':
+                return '"'
+            case '&amp;':
+                return '&'
+        }
+    })
+}
+
+// 定义一个补零的函数
+function padZero(n) {
+    return n > 9 ? n : '0' + n
+}
+
+module.exports = {
+    dateFormat,
+    htmlEscape,
+    htmlUnEscape
+}
---
+++ node.js-day-3/text.js
@@ -0,0 +1,3 @@
+const time = require('./ithema-mwzm/index')
+const da = time.dateFormat(new Date())
+console.log(da);
